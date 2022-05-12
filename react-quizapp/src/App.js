7e50ed7cf8c92f157881f0d592115f16a7c60175
Vikms95 <vms1995@hotmail.com>
2022-05-12 06:42:52 +0200

    Implement API fetch with useEffect

--- react-quizapp/src/App.js
+++ react-quizapp/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// https://opentdb.com/api_config.php
+
+import React from 'react';
+import Question from './components/Question';
 import './App.css';
 
 function App() {
+    
+    const [questions, setQuestions] = React.useState([])
+
+    /*
+    useEffect to retrieve 10 questions, only will be triggered when:
+        - Page is loaded
+        - New game button is clicked
+    */
+   React.useEffect(() =>{
+        async function fetchQuestions(){
+            const response = await fetch('https://opentdb.com/api.php?amount=10')
+            const data = await response.json()
+            setQuestions(data.results)
+        }
+        fetchQuestions()
+        console.log(questions[0].difficulty)
+    }
+    ,[]
+    ) 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Question />
     </div>
   );
 }
